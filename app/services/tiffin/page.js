f491631e3c7a2f764126ce28ece2f81ef08128af
pankaj ram pawar <pankajpawars123@gmail.com>
2025-01-13 08:25:00 +0530

    tiffin page added

--- app/services/tiffin/page.js
+++ app/services/tiffin/page.js
@@ -1,7 +1,41 @@
+import TiffinCard from "@/app/ui/components/tiffin/tiffinCard";
+
 export default function Tiffin() {
     return (
         <div>
-            tiffin page
+            {/* hero section */}
+            <section>
+                <div>
+                    <h1>Budget Friendly And Truly Good Meal.</h1>
+                </div>
+
+                <div>
+                    Location
+                </div>
+            </section>
+
+            {/* items section */}
+            <section>
+                {/* section for filters */}
+                <section>
+                    <div>
+                        Veg
+                    </div>
+                    <div>
+                        Non Veg
+                    </div>
+                    <div>
+                        Filter
+                    </div>
+                </section>
+
+                {/* items list */}
+                <section>
+                    <TiffinCard
+
+                    />
+                </section>
+            </section>
         </div>
     )
-}
+}   
