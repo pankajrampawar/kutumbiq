73fe2d6ca24d5f5f283103be2d4d99d95b6ec84b
pankaj ram pawar <pankajpawars123@gmail.com>
2025-01-13 08:25:18 +0530

    upadted tiffin card holder

---
+++ app/ui/components/tiffin/tiffinCard.js
@@ -0,0 +1,59 @@
+'use client'
+import { useState } from "react"
+
+export default function TiffinCard({ title, price, description, src, alt, serviceProvider }) {
+
+    const [itemsInCart, setItemsInCart] = useState(0);
+
+    const addItemToCart = () => {
+        setItemsInCart(prev => prev + 1);
+    };
+
+    const removeItemFromCart = () => {
+        setItemsInCart((prev) => {
+            if (prev === 0) return 0;
+            else {
+                return prev - 1;
+            }
+        });
+    };
+
+    return (
+        <div>
+            <section>
+                <article>
+                    <h2>{title}</h2>
+                    <p><span>{price}</span></p>
+                    <p>{description}</p>
+                </article>
+
+                <div>
+                    <img
+                        src={src}
+                        alt={alt}
+                    />
+                    <div>
+                        {itemsInCart &&
+                            <>
+                                <button onClick={addItemToCart}><span>-</span></button>
+                                <button onClick={removeItemFromCart}><span>+</span></button>
+                            </>
+                        }
+
+                        {!itemsInCart &&
+                            <button onClick={addItemToCart}>
+                                <p>Add</p>
+                            </button>
+                        }
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div>
+                    <p>By: {serviceProvider}</p>
+                </div>
+            </section>
+        </div>
+    )
+}
