6d17f23271df1d681659c6f9ee6e97520623cadb
pankaj ram pawar <pankajpawars123@gmail.com>
2025-01-16 05:43:49 +0530

    improved routing for the empty cart page'

--- app/services/tiffin/cart/page.js
+++ app/services/tiffin/cart/page.js
@@ -3,16 +3,28 @@
 import { useCart } from "@/app/context/cartContext"
 import CheckoutCrumb from "@/app/ui/components/tiffin/checkoutCrumb";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function CartPage() {
 
+    const router = useRouter();
     const { cartItems, addItemToCart, removeItemFromCart, serviceProviderInCart } = useCart();
 
     return (
-        <div className="mx-[5%]">
-            <h1 className="text-xl">Checkout Page</h1>
+        <div className="mx-[3%]">
+            <h1 className="text-xl mt-2 underline underline-offset-4 ">Checkout Page</h1>
             <div className="mt-10">
-                {
+                {cartItems.length === 0
+                    ?
+                    <div className="w-full h-full fixed top-0 justify-center items-center flex flex-col gap-5">
+                        <p>
+                            No items in cart!
+                        </p>
+                        <button className="bg-black text-white px-4 py-2 rounded-lg" onClick={() => router.back()}>
+                            Go back
+                        </button>
+                    </div>
+                    :
                     cartItems.map((item) => {
                         return (
                             <div key={item.id} className="flex justify-between w-full">
--- app/ui/components/tiffin/checkoutCrumb.js
+++ app/ui/components/tiffin/checkoutCrumb.js
@@ -4,15 +4,18 @@ import { useCart } from "@/app/context/cartContext"
 export default function CheckoutCrumb({ item }) {
 
     const { addItemToCart, removeItemFromCart } = useCart();
+
     return (
         <div className="w-full flex justify-between">
             <h3 className="text-lg">{item.title}</h3>
-            <div className="flex gap-3 bg-slate-200 p-1 rounded-lg text-sm">
-                <button onClick={() => removeItemFromCart(item.id)}><span>-</span></button>
-                <p>{item.quantity}</p>
-                <button onClick={() => addItemToCart(item)}><span>+</span></button>
-            </div>
-            <p>{item.price}</p>
+            <section className="flex gap-4">
+                <div className="flex gap-3 bg-slate-200 p-1 rounded-lg text-sm">
+                    <button onClick={() => removeItemFromCart(item.id)}><span>-</span></button>
+                    <p>{item.quantity}</p>
+                    <button onClick={() => addItemToCart(item)}><span>+</span></button>
+                </div>
+                <p>{item.price * item.quantity}</p>
+            </section>
         </div>
     )
 }
