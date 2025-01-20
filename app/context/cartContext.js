3b2a1658255cba0bf23634aa0be2882928ecaf6b
pankaj ram pawar <pankajpawars123@gmail.com>
2025-01-20 12:57:45 +0530

    improved cart context

--- app/context/cartContext.js
+++ app/context/cartContext.js
@@ -94,7 +94,14 @@ export const CartProvider = ({ children }) => {
             {/* Animated Checkout Bar */}
             <motion.div
                 initial={{ y: "100%" }}
-                animate={{ y: pathname === "/services/tiffin/cart" && "/services/tiffin/confirmOrder" && "/form/address" && "/form/phoneNumber" ? "100%" : cartItems.length > 0 ? 0 : "100%" }}
+                animate={{
+                    y:
+                        ["/services/tiffin/cart", "/services/tiffin/confirmOrder", "/form/address", "/form/phoneNumber"].includes(pathname)
+                            ? "100%"
+                            : cartItems.length > 0
+                                ? 0
+                                : "100%",
+                }}
                 transition={{ duration: 0.5, ease: "easeInOut" }}
                 className={clsx(
                     "fixed bottom-0 left-0 w-screen px-2 pb-2 flex justify-between items-center",
