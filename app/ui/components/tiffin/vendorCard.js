816fc5d0207280cf1292a6631b89a25999383761
pankaj ram pawar <pankajpawars123@gmail.com>
2025-02-13 17:11:44 +0530

    Updated shaky motion

--- app/ui/components/tiffin/vendorCard.js
+++ app/ui/components/tiffin/vendorCard.js
@@ -4,7 +4,7 @@ import { Star, Clock, IndianRupee } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { montserrat, lato } from "../../fonts";
 import { getCurrentTimeStatus } from "@/app/actions/tiffinActions";
-import { Montserrat } from "next/font/google";
+import { motion } from "framer-motion";
 
 export default function VendorCard({
     image,
@@ -24,6 +24,17 @@ export default function VendorCard({
         router.push(`/services/tiffin/menu/${id}`);
     };
 
+    const shakeAnimation = {
+        inView: {
+            x: [0, -8, 8, -8, 8, -8, 8, -8, 0], // More pronounced horizontal movement
+            scale: [1, 1.05, 1, 1.05, 1, 1.05, 1, 1.05, 1], // Slight scaling for emphasis
+            transition: {
+                duration: 1.2, // Total duration of the animation
+                ease: "easeInOut", // Smoother easing
+                times: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 1], // Evenly spaced keyframes
+            },
+        },
+    };
 
     console.log(filter)
     return (
@@ -36,18 +47,27 @@ export default function VendorCard({
                 className={`bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl relative ${isActive ? "" : "hover:shadow-md"
                     }`}
             >
-                <div className="absolute top-0 right-0">
+                <div className="absolute top-0 right-0 z-10">
                     <span className={` ${montserrat.className} p-2 px-3 text-[14px] font-bold text-white rounded bg-gradient-to-b from-rustOrange/70 via-rustOrange to-rustOrange/70 rounded-bl-[20px]`}>
                         25% OFF <span className="text-lg ml-2">🎉</span>
                     </span>
                 </div>
-
-                <img
-                    src={image}
-                    alt={name}
-                    className={`w-full h-48 object-cover ${isActive ? "" : "grayscale"
-                        }`}
-                />
+                <div className="relative">
+                    <img
+                        src={image}
+                        alt={name}
+                        className={`w-full h-48 object-cover ${isActive ? "" : "grayscale"
+                            }`}
+                    />
+                    <motion.div
+                        whileInView="inView"
+                        viewport={{ once: true }}
+                        variants={shakeAnimation}
+                        transition={{ delay: 0.6 }}
+                        className={` ${name === "Nallii's Kitchen" ? "" : "hidden"} absolute  text-white p-1 bottom-0 ${montserrat.className} text-base font-medium  bg-gradient-to-b from-green-700/70 via-green-700 to-green-700/70`}>
+                        Best in Tiffins 🏆
+                    </motion.div>
+                </div>
                 <div className="p-4">
                     <h3
                         className={`text-xl font-semibold mb-2 ${isActive ? "text-gray-900" : "text-gray-500"
